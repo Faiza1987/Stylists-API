22a5bc143bdf972313ebf6ee067b389d39541959
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-07-09 13:54:35 -0700

    Updated database schema and can now make post requests to DRF using DRF interface

--- api/models.py
+++ api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
-from django.contrib.postgres.fields import ArrayField
 
 
 class User(AbstractUser):
@@ -16,10 +15,12 @@ class User(AbstractUser):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    full_name = models.CharField(max_length=25)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='profile')
+
     phone_number = models.CharField(max_length=10)
-    years_experience = models.IntegerField()
+    years_experience = models.IntegerField(default=0)
     licenses = models.ImageField(upload_to='uploads', blank=True)
     photo1 = models.ImageField(upload_to='uploads', blank=True)
     photo2 = models.ImageField(upload_to='uploads', blank=True)
@@ -27,8 +28,7 @@ class UserProfile(models.Model):
     photo4 = models.ImageField(upload_to='uploads', blank=True)
     photo5 = models.ImageField(upload_to='uploads', blank=True)
     photo6 = models.ImageField(upload_to='uploads', blank=True)
-    # holds a List of specializations
-    specializations = ArrayField(models.CharField(max_length=25, blank=True))
+    specializations = models.TextField(max_length= 1000)
 
 
 
--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,8 @@ from api.models import User, UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('full_name', 'phone_number', 'licenses', 'photo1',
+        fields = ('phone_number', 'years_experience', 'licenses',
+                  'photo1',
                   'photo2', 'photo3', 'photo4', 'photo5', 'photo6',
                   'specializations')
 
@@ -19,31 +20,25 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
                   'password', 'profile')
         extra_kwargs = {'password': {'write_only': True}}
 
-
     def create(self, validated_data):
         profile_data = validated_data.pop('profile')
         password = validated_data.pop('password')
         user = User(**validated_data)
         user.set_password(password)
-
         user.save()
-
         UserProfile.objects.create(user=user, **profile_data)
-
         return user
-
 
     def update(self, instance, validated_data):
         profile_data = validated_data.pop('profile')
         profile = instance.profile
         instance.email = validated_data.get('email', instance.email)
-
         instance.save()
 
-        profile.full_name = profile_data.get('full_name',
-                                             profile.full_name)
         profile.phone_number = profile_data.get('phone_number',
                                                 profile.phone_number)
+        profile.years_experience = profile_data.get(
+            'years_experience', profile.years_experience)
         profile.licenses = profile_data.get('licenses',
                                             profile.licenses)
         profile.photo1 = profile_data.get('photo1', profile.photo1)
@@ -54,7 +49,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         profile.photo6 = profile_data.get('photo6', profile.photo6)
         profile.specializations = profile_data.get(
             'specializations', profile.specializations)
-
         profile.save()
-
         return instance
+
+
+
